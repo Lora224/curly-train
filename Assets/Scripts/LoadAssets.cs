f2847df93142a49a8a073ccbc10d7002d7c5c4f5
Lora Lyu <martialmask@gmail.com>
2024-08-26 20:06:05 +1000

    a

--- Assets/Scripts/LoadAssets.cs
+++ Assets/Scripts/LoadAssets.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class LoadAssets : MonoBehaviour
 {
-    public GameObject redObj = new GameObject();
+    public GameObject redObj;
   
     // Start is called before the first frame update
     void Start()
